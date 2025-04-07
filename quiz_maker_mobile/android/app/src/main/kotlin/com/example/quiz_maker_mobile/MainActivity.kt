0ff0ed2d7ff2fa977314417ee351908a5b39d891
Yassine <yassinemiladi21@gmail.com>
2025-04-07 10:46:15 +0100

    replace two apps by one shared app

--- quiz_maker_mobile/android/app/src/main/kotlin/com/example/quiz_maker_mobile/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.quiz_maker_mobile
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- quiz_maker_web/android/app/src/main/kotlin/com/example/quiz_maker_web/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.quiz_maker_web
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
